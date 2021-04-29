8aa94e5b6b58685707d514eaaf8ba5ee03aec330
Junior Oliveira Dias de Souza <junior.oliveira@bancointer.com.br>
2021-04-29 14:00:12 -0300

    Initial commit

---
+++ src/main/java/br/com/itauteste/itautestevalidarsenhasevice/ItautesteValidarSenhaSeviceApplication.java
@@ -0,0 +1,13 @@
+package br.com.itauteste.itautestevalidarsenhasevice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItautesteValidarSenhaSeviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItautesteValidarSenhaSeviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/itauteste/itautestevalidarsenhasevice/ItautesteValidarSenhaSeviceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.itauteste.itautestevalidarsenhasevice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItautesteValidarSenhaSeviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
