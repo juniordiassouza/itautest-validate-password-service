91ef27ea515d3ff91a16c1bb82c630e95ea66643
Junior Oliveira Dias de Souza <junior.oliveira@bancointer.com.br>
2021-04-29 21:18:20 -0300

    varias correcoes

--- src/main/java/br/com/itauteste/controller/SenhaController.java
+++ src/main/java/br/com/itauteste/controller/SenhaController.java
@@ -1,15 +1,24 @@
 package br.com.itauteste.controller;
 
+import br.com.itauteste.models.Senha;
+import io.swagger.annotations.*;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/book")
+@Api(value = "Senha")
 public class SenhaController {
 
-    @PutMapping("/validaSenha/{senha}")
-    @ResponseStatus(HttpStatus.OK)
-    public Boolean verificaValidadeSenha(@PathVariable(value = "senha") String senha){
+    @ApiOperation(value = "Metodo responsavel por verificar se uma senha está no padrão exigido", nickname = "validarSenha", notes="Senha Para ser validadada", tags = { })
+    @ApiResponses(value = {
+    @ApiResponse(code= 200, message = "Dados consultados com sucesso"),
+    @ApiResponse(code= 400, message = "Erro requisição", response = Error.class),
+    @ApiResponse(code= 404, message = "Recurso não encontrado", response = Error.class),
+    @ApiResponse(code= 500, message = "Erro interno", response = Error.class)
+    })
+    @RequestMapping(value="/senha", produces = {"application/json"}, method = RequestMethod.POST)
+    public Boolean verificaValidadeSenha(@ApiParam(value = "Senha", required = true) @Validated @RequestBody Senha senha){
 
 
         return null;
---
+++ src/main/java/br/com/itauteste/models/Senha.java
@@ -0,0 +1,12 @@
+package br.com.itauteste.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Senha {
+
+    private String senha;
+
+}
